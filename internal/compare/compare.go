fc1f5b3ea3e21d3bffcff127d376df7678c260ed
Takashi Yamashina <takashi.yamashina@gmail.com>
2020-12-27 20:26:08 +0900

    Refactor LessThan(value1 interface {}, value2 interface {}) #1

--- internal/compare/compare.go
+++ internal/compare/compare.go
@@ -9,71 +9,41 @@ func EqualTo(value1 interface{}, value2 interface{}) bool {
 
 // LessThan returns true if value1 < value2.
 func LessThan(value1 interface{}, value2 interface{}) bool {
-	switch value1.(type) {
-	case float32, float64:
-		return lessThanFloat(value1, value2)
-	case int, int8, int16, int32, int64:
-		return lessThanInt(value1, value2)
-	case uint, uint8, uint16, uint32, uint64:
-		return lessThanUint(value1, value2)
-	case string:
-		return lessThanString(value1, value2)
+	type1 := reflect.ValueOf(value1).Kind()
+	type2 := reflect.ValueOf(value2).Kind()
+	v1 := reflect.ValueOf(value1)
+	v2 := reflect.ValueOf(value2)
+
+	if isFloat(type1) || isFloat(type2) {
+		return v1.Float() < v2.Float()
+	} else if isInt(type1) || isInt(type2) {
+		return v1.Int() < v2.Int()
+	} else if isUint(type1) || isUint(type2) {
+		return v1.Uint() < v2.Uint()
+	} else if isString(type1) || isString(type2) {
+		return v1.String() < v2.String()
 	}
+
 	return false
+}
+
+func isFloat(kind reflect.Kind) bool {
+	return kind == reflect.Float32 || kind == reflect.Float64
+}
+
+func isInt(kind reflect.Kind) bool {
+	return kind == reflect.Int || kind == reflect.Int8 || kind == reflect.Int16 || kind == reflect.Int32
+}
+
+func isUint(kind reflect.Kind) bool {
+	return kind == reflect.Uint || kind == reflect.Uint8 || kind == reflect.Uint16 || kind == reflect.Uint32
+}
+
+func isString(kind reflect.Kind) bool {
+	return kind == reflect.String
 }
 
 // GreaterThan returns true if value1 > value2.
 func GreaterThan(value1 interface{}, value2 interface{}) bool {
 	return LessThan(value2, value1)
-}
-
-func lessThanFloat(value1, value2 interface{}) bool {
-	v1 := reflect.ValueOf(value1)
-	v2 := reflect.ValueOf(value2)
-	switch value2.(type) {
-	case float32, float64:
-		return v1.Float() < v2.Float()
-	case int, int8, int16, int32, int64:
-		return v1.Float() < v2.Float()
-	case uint, uint8, uint16, uint32, uint64:
-		return v1.Float() < v2.Float()
-	default:
-		return false
-	}
-}
-
-func lessThanInt(value1, value2 interface{}) bool {
-	v1 := reflect.ValueOf(value1)
-	v2 := reflect.ValueOf(value2)
-	switch value2.(type) {
-	case float32, float64:
-		return v1.Float() < v2.Float()
-	case int, int8, int16, int32, int64:
-		return v1.Int() < v2.Int()
-	case uint, uint8, uint16, uint32, uint64:
-		return v1.Int() < v2.Int()
-	default:
-		return false
-	}
-}
-
-func lessThanUint(value1, value2 interface{}) bool {
-	v1 := reflect.ValueOf(value1)
-	v2 := reflect.ValueOf(value2)
-	switch value2.(type) {
-	case float32, float64:
-		return v1.Float() < v2.Float()
-	case int, int8, int16, int32, int64:
-		return v1.Int() < v2.Int()
-	case uint, uint8, uint16, uint32, uint64:
-		return v1.Uint() < v2.Uint()
-	default:
-		return false
-	}
-}
-
-func lessThanString(value1, value2 interface{}) bool {
-	v1 := reflect.ValueOf(value1)
-	v2 := reflect.ValueOf(value2)
-	return v1.String() < v2.String()
 }
