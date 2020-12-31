154e81ab60b1f0b4a8dc0bcaffac6c35edb7d351
Takashi Yamashina <takashi.yamashina@gmail.com>
2021-01-01 04:15:45 +0900

    change Fatal to Error

--- that.go
+++ that.go
@@ -9,6 +9,6 @@ import (
 // That is asserts that actual satisfies the condition specified by matcher.
 func That(t *testing.T, actual interface{}, expected *matcher.Matcher, msgAndArgs ...interface{}) {
 	if !expected.Match(actual) {
-		t.Fatal(buildMessage("Assertion error", expected.Describe, actual, msgAndArgs...))
+		t.Error(buildMessage("Assertion error", expected.Describe, actual, msgAndArgs...))
 	}
 }
